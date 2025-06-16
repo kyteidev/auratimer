683e25ba291239cd2020e8d706c833f29c6c7244
kyteidev <89528285+kyteidev@users.noreply.github.com>
2025-06-16 23:49:32 +0300

    style: make animation last 200ms2

--- src/components/timer.rs
+++ src/components/timer.rs
@@ -55,20 +55,20 @@ pub fn Timer() -> Element {
         div {
             class: "relative bg-transparent w-3/5 h-2/5 rounded-lg text-[10rem] flex items-center justify-center",
             div {
-                class: "transition duration-100 absolute top-0 left-0 w-full h-full opacity-10 rounded-lg bg-transparent hover:bg-gray-500 z-10 cursor-pointer flex items-center justify-center",
+                class: "transition duration-200 absolute top-0 left-0 w-full h-full opacity-10 rounded-lg bg-transparent hover:bg-gray-500 z-10 cursor-pointer flex items-center justify-center",
                 onclick: toggle_timer,
                 onmouseenter: move |_| hovering.set(true),
                 onmouseleave: move |_| hovering.set(false),
             }
             Icon {
                 icon_type: if *timer_running.read() { IconType::Pause } else { IconType::Start },
-                class: "transition duration-100 absolute",
+                class: "transition duration-200 absolute",
                 opacity: if *hovering.read() { 1.0 } else { 0.0 },
                 color: "#000000",
                 size: "96px",
             }
             p {
-                class: "transition duration-100",
+                class: "transition duration-200",
                 opacity: "{opacity}",
                 "{formatted_time}"
             }
