0070868aa6fc65791b53f714bad5f28ccd7dde00
kyteidev <89528285+kyteidev@users.noreply.github.com>
2025-06-16 23:46:53 +0300

    feat: add icon for pause/resume

---
+++ src/components/icons.rs
@@ -0,0 +1,51 @@
+// SVG icons from Tabler Icons (https://tabler.io/icons). SVG icons are licensed under the MIT License.
+
+use dioxus::prelude::*;
+
+#[derive(Clone, PartialEq)]
+pub enum IconType {
+    Start,
+    Pause,
+}
+
+#[derive(Props, Clone, PartialEq)]
+pub struct Props {
+    icon_type: IconType,
+    class: String,
+    opacity: f32,
+    color: String,
+    size: String,
+}
+
+#[component]
+pub fn Icon(props: Props) -> Element {
+    let children = match props.icon_type {
+        IconType::Start => rsx!(
+            path {
+                d: "M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z"
+            }
+        ),
+        IconType::Pause => rsx!(
+            path {
+                d: "M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
+            }
+            path {
+                d: "M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z"
+            }
+        ),
+    };
+
+    rsx!(
+        svg {
+            xmlns: "http://www.w3.org/2000/svg",
+            view_box: "0 0 24 24",
+            fill: "{props.color}",
+            class: "{props.class}",
+            width: "{props.size}",
+            height: "{props.size}",
+            opacity: "{props.opacity}",
+            path { d: "M0 0h24v24H0z", fill: "none" }
+            {children}
+        }
+    )
+}
--- src/components/timer.rs
+++ src/components/timer.rs
@@ -1,9 +1,13 @@
 use dioxus::prelude::*;
 
+use crate::components::icons::{Icon, IconType};
+
 #[component]
 pub fn Timer() -> Element {
     let mut timer_running = use_signal(|| false);
     let mut seconds_remaining = use_signal(|| 25 * 60);
+
+    let mut hovering = use_signal(|| false);
 
     let formatted_time = {
         let minutes = *seconds_remaining.read() / 60;
@@ -45,14 +49,27 @@ pub fn Timer() -> Element {
         ()
     });
 
+    let opacity = if *hovering.read() { 0.1 } else { 1.0 };
+
     rsx! {
         div {
             class: "relative bg-transparent w-3/5 h-2/5 rounded-lg text-[10rem] flex items-center justify-center",
             div {
-                class: "transition duration-100 absolute top-0 left-0 w-full h-full opacity-10 rounded-lg bg-transparent hover:bg-gray-500 z-10 cursor-pointer",
+                class: "transition duration-100 absolute top-0 left-0 w-full h-full opacity-10 rounded-lg bg-transparent hover:bg-gray-500 z-10 cursor-pointer flex items-center justify-center",
                 onclick: toggle_timer,
+                onmouseenter: move |_| hovering.set(true),
+                onmouseleave: move |_| hovering.set(false),
+            }
+            Icon {
+                icon_type: if *timer_running.read() { IconType::Pause } else { IconType::Start },
+                class: "transition duration-100 absolute",
+                opacity: if *hovering.read() { 1.0 } else { 0.0 },
+                color: "#000000",
+                size: "96px",
             }
             p {
+                class: "transition duration-100",
+                opacity: "{opacity}",
                 "{formatted_time}"
             }
         }
